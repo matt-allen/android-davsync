e0bb8c14017f0df16aef8c61ff6ad7a7f2eb5c91
Matt Allen <mattallen092@gmail.com>
2018-04-10 00:02:35 +0100

    Created a main activity to control the app

---
+++ app/src/main/java/net/zekjur/davsync/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.zekjur.davsync.activity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Entry point of the application where the user can control
+ * their settings, server config etc.
+ *
+ * @author Matt Allen
+ */
+class MainActivity: AppCompatActivity()
+{
+}
