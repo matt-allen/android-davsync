b2ee0e21ad94e851cd274d71acf437cee67f4919
Matt Allen <mattallen092@gmail.com>
2018-04-09 23:41:17 +0100

    Cut down the network received a bit

--- app/src/main/java/net/zekjur/davsync/NetworkReceiver.java
+++ app/src/main/java/net/zekjur/davsync/NetworkReceiver.java
@@ -1,58 +1,59 @@
 package net.zekjur.davsync;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.util.Log;
 
-public class NetworkReceiver extends BroadcastReceiver {
+import net.zekjur.davsync.configuration.Configuration;
+import net.zekjur.davsync.configuration.ConfigurationFactory;
 
+public class NetworkReceiver extends BroadcastReceiver
+{
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(Context context, Intent intent)
+	{
 		Log.d("davsync", "network connectivity changed");
 
 		if (!ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction()))
-			return;
-
-		ConnectivityManager cs = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-		NetworkInfo info = cs.getActiveNetworkInfo();
-		if (info == null || !info.isConnected()) {
-			Log.d("davsync", "_NOT_ connected anymore, not doing anything.");
+		{
 			return;
 		}
 
-		SharedPreferences preferences = context.getSharedPreferences("net.zekjur.davsync_preferences",
-				Context.MODE_PRIVATE);
+		ConnectivityManager cs = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo info = cs.getActiveNetworkInfo();
+		if (info == null || !info.isConnected())
+		{
+			// Not connected anymore
+			return;
+		}
 
-		boolean syncOnWifiOnly = preferences.getBoolean("auto_sync_on_wifi_only", true);
-
-		if (syncOnWifiOnly && !(ConnectivityManager.TYPE_WIFI == info.getType())) {
-			Log.d("davsync", "Not on WIFI, not doing anything.");
+		boolean wifiOnly = getConfiguration(context).isWiFiOnly();
+		if (wifiOnly && !(ConnectivityManager.TYPE_WIFI == info.getType()))
+		{
+			Log.d("davsync", "Not on WiFi, not doing anything.");
 			return;
 		}
 
 		Log.d("davsync", "Checking whether pictures need to be synced");
 
-		// XXX: It doesn’t really feel right to do this blockingly in a
-		// BroadcastReceiver, but I was unable to find whether this is the right
-		// way or whether there is a better one.
-
 		DavSyncOpenHelper helper = new DavSyncOpenHelper(context);
-		ArrayList<String> uris = helper.getQueuedUris();
-		for (String uri : uris) {
-			Intent ulIntent = new Intent(context, UploadService.class);
-			// evtl: mapintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-			ulIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(uri));
-			context.startService(ulIntent);
+		List<String> uris = helper.getQueuedUris();
+		for (String uri : uris)
+		{
+			Intent uploadIntent = new Intent(context, UploadService.class);
+			uploadIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(uri));
+			context.startService(uploadIntent);
 		}
 	}
 
+	private Configuration getConfiguration(Context context)
+	{
+		return new ConfigurationFactory(context).getConfiguration();
+	}
 }
