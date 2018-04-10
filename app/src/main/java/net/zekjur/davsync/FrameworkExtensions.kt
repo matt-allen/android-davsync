605da861a4f54c668a605b9413b9e87c0c680291
Matt Allen <mattallen092@gmail.com>
2018-04-10 20:42:29 +0100

    Added some nice framework extensions

---
+++ app/src/main/java/net/zekjur/davsync/FrameworkExtensions.kt
@@ -0,0 +1,16 @@
+package net.zekjur.davsync
+
+import android.text.TextUtils
+import android.util.Patterns
+
+// String extensions
+fun String.isInt(): Boolean = TextUtils.isDigitsOnly(this)
+fun String.isEmail(): Boolean = Patterns.EMAIL_ADDRESS.matcher(this).matches()
+fun String.isIpv4(): Boolean = Patterns.IP_ADDRESS.matcher(this).matches()
+fun String.isUrl(): Boolean = Patterns.WEB_URL.matcher(this).matches()
+
+// Int extensions
+fun Int.secondsInMs(): Long = (this * 1000).toLong()
+fun Int.minutesInMs(): Long = secondsInMs() * 60
+fun Int.hoursInMs(): Long = minutesInMs() * 60
+fun Int.daysInMs(): Long = hoursInMs() * 24
