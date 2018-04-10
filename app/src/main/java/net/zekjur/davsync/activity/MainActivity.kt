bb2cf0a8eb574dab6b07d8a224c5384193a8038a
Matt Allen <mattallen092@gmail.com>
2018-04-10 22:03:34 +0100

    The MainActivity is now fully implemented

--- app/src/main/java/net/zekjur/davsync/activity/MainActivity.kt
+++ app/src/main/java/net/zekjur/davsync/activity/MainActivity.kt
@@ -1,8 +1,14 @@
 package net.zekjur.davsync.activity
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 import net.zekjur.davsync.R
+import net.zekjur.davsync.fragment.ServerDetailsFragment
+import net.zekjur.davsync.fragment.SettingsFragment
 
 /**
  * Entry point of the application where the user can control
@@ -10,11 +16,25 @@ import net.zekjur.davsync.R
  *
  * @author Matt Allen
  */
-class MainActivity: AppCompatActivity()
+class MainActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener
 {
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		bottom_bar?.setOnNavigationItemSelectedListener(this)
+		setFragment(ServerDetailsFragment())
 	}
+
+	override fun onNavigationItemSelected(item: MenuItem): Boolean
+	{
+		when (item.itemId)
+		{
+			R.id.action_server -> setFragment(ServerDetailsFragment())
+			R.id.action_settings -> setFragment(SettingsFragment())
+		}
+		return true
+	}
+
+	private fun setFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().replace(R.id.fragment, fragment).commit()
 }
