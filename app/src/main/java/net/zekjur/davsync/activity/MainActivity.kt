c5719bd30f468c1e13bc2a24637350c5248f49ac
Matt Allen <mattallen092@gmail.com>
2018-04-10 20:41:33 +0100

    Added bottom bar to the main activity

--- app/src/main/java/net/zekjur/davsync/activity/MainActivity.kt
+++ app/src/main/java/net/zekjur/davsync/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package net.zekjur.davsync.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import net.zekjur.davsync.R
 
 /**
  * Entry point of the application where the user can control
@@ -10,4 +12,9 @@ import android.support.v7.app.AppCompatActivity
  */
 class MainActivity: AppCompatActivity()
 {
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
 }
