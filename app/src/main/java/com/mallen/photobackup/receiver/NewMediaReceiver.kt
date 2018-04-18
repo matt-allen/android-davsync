acd2d501433e785ff6ae44fb11726502fdc864f3
Matt Allen <mattallen092@gmail.com>
2018-04-19 00:01:33 +0100

    Implemented the JobScheduler API for waiting to upload images to the WebDAV server if on metered network

--- app/src/main/java/com/mallen/photobackup/receiver/NewMediaReceiver.kt
+++ app/src/main/java/com/mallen/photobackup/receiver/NewMediaReceiver.kt
@@ -1,51 +1,27 @@
 package com.mallen.photobackup.receiver
 
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.mallen.photobackup.model.QueuedTask
-import com.mallen.photobackup.queue.QueueManager
+import com.mallen.photobackup.configuration.Configuration
+import com.mallen.photobackup.configuration.ConfigurationFactory
 import com.mallen.photobackup.service.UploadService
 
-class NewMediaReceiver : BaseBroadcastReceiver()
+class NewMediaReceiver : BroadcastReceiver()
 {
-	private val tag = NewMediaReceiver::class.java.simpleName
-
 	override fun onReceive(context: Context, intent: Intent)
 	{
 		val isNewPic = android.hardware.Camera.ACTION_NEW_PICTURE == intent.action
 		val isNewVid = android.hardware.Camera.ACTION_NEW_VIDEO == intent.action
 
-		if (!isNewPic && !isNewVid)
-		{
-			return
-		}
-
-		if (isNewPic && !getConfiguration(context).isUploadingImages())
-		{
-			Log.d(tag, "User does not want images uploaded")
-			return
-		}
-
-		if (isNewVid && !getConfiguration(context).isUploadingVideos())
-		{
-			Log.d(tag, "User does not want videos uploaded")
-			return
-		}
-
 		val uri = intent.data
-		if (shouldUpload(context))
+		if ((isNewPic && getConfiguration(context).isUploadingImages()) || (isNewVid && getConfiguration(context).isUploadingVideos()))
 		{
-			Log.d(tag, "Trying to upload $uri immediately (on WiFi)")
 			val ulIntent = Intent(context, UploadService::class.java)
 			ulIntent.putExtra(Intent.EXTRA_STREAM, uri)
 			context.startService(ulIntent)
 		}
-		else
-		{
-			Log.d(tag, "Queueing $uri for later (not on WiFi)")
-			// otherwise, queue the image for later
-			QueueManager.addToQueue(context, QueuedTask(uri))
-		}
 	}
+
+	private fun getConfiguration(context: Context?): Configuration = ConfigurationFactory(context).getConfiguration()
 }
--- app/src/main/java/com/mallen/photobackup/service/UploadService.kt
+++ app/src/main/java/com/mallen/photobackup/service/UploadService.kt
@@ -1,9 +1,18 @@
 package com.mallen.photobackup.service
 
 import android.app.IntentService
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.net.Uri
 import com.mallen.photobackup.UploadFileTask
+import com.mallen.photobackup.configuration.Configuration
+import com.mallen.photobackup.configuration.ConfigurationFactory
+import com.mallen.photobackup.model.QueuedTask
+import com.mallen.photobackup.queue.QueueManager
 
 
 /**
@@ -16,7 +25,33 @@ class UploadService: IntentService("UploadService")
 		val uri = intent?.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
 		if (uri != null)
 		{
-			UploadFileTask(this, uri).execute()
+			if (isMeteredNetwork() && getConfiguration().isWiFiOnly())
+			{
+				QueueManager.addToQueue(this, QueuedTask(uri))
+				schedulePendingUploads()
+			}
+			else
+			{
+				UploadFileTask(this, uri).execute()
+			}
 		}
 	}
+
+	/**
+	 * If we're on a metered network and the user doesn't want to use up their allowance,
+	 * schedule this to happen when we're on WiFi/Ethernet etc.
+	 */
+	private fun schedulePendingUploads()
+	{
+		val scheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as? JobScheduler
+		val job = JobInfo.Builder(0, ComponentName(this, UploadService::class.java))
+				.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+				.setRequiresCharging(true)
+				.build()
+		scheduler?.schedule(job)
+	}
+
+	private fun isMeteredNetwork(): Boolean = getConnectivityManager().isActiveNetworkMetered
+	private fun getConfiguration(context: Context? = this): Configuration = ConfigurationFactory(context).getConfiguration()
+	private fun getConnectivityManager(context: Context? = this): ConnectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 }
